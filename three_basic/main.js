efd4ece632c2bc9edecdcebed4c7b46e54bdb1fb
Sahil lakhani <sahillakhani266@gmail.com>
2024-09-12 21:40:06 +0530

    added basic scene

--- three_basic/main.js
+++ three_basic/main.js
@@ -1,24 +1,34 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('canvas')
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene()
+
+scene.background = new THREE.Color("#F0F0F0");
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 5
+ 
+const geometry = new THREE.DodecahedronGeometry()
+const material = new THREE.MeshBasicMaterial({color:'#4658d5'})
+
+const dodecahedron = new THREE.Mesh(geometry, material)
+
+const boxGeometry = new THREE.BoxGeometry(2,0.1,2)
+const boxMaterial = new THREE.MeshBasicMaterial({color: '#B4B4B4'})
+
+const box = new THREE.Mesh(boxGeometry, boxMaterial)
+box.position.y = -1.5
+
+scene.add(dodecahedron, box)
+
+const light = new THREE.SpotLight(0x006769, 100)
+light.position.set(1,1,1)
+
+scene.add(light)
+
+const renderer = new THREE.WebGLRenderer({canvas})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+renderer.render(scene, camera)
